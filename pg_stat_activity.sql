945b95d815844095dbc63f7a47d78711707d1d31
Emerson Martins <emersonmdba@gmail.com>
2019-07-16 16:37:14 -0300

    Add files via upload

---
+++ pg_stat_activity.sql
@@ -0,0 +1,19 @@
+﻿SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'siapnet'
+	AND pid <> pg_backend_pid()
+	AND state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled') 
+	AND state_change < current_timestamp - INTERVAL '1' MINUTE;
+	
+	
+	SELECT 
+    pg_terminate_backend(pid) 
+FROM 
+    pg_stat_activity 
+WHERE 
+    -- don't kill my own connection!
+    pid <> pg_backend_pid()
+    -- don't kill the connections to other databases
+AND datname = 'siapnet';
+
+	
---
+++ pg_stat_activity_2.sql
@@ -0,0 +1,11 @@
+SELECT * FROM pg_stat_activity
+
+SELECT count(*) FROM pg_stat_activity WHERE datname = 'siapnet'
+
+SELECT
+  pid,
+  now() -  pg_stat_activity.query_start AS duration,
+  query,
+  state
+FROM pg_stat_activity
+WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
